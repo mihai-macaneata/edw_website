08a541b95d70af117136f96014cd256ddb1b26be
MadelaineS <madalina.sp21@gmail.com>
2019-10-03 10:23:07 +0300

    changed map with static img

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -489,15 +489,11 @@
               </b-container>
             </b-col>
             <!-- Google Maps EDW -->
-            <b-col data-aos="fade-up" lg="6">
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2847.39751858446!2d26.08024471574234!3d44.46602390750737!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40b2021b499f3bfb%3A0xfc73471ea7cf8a74!2sEau%20de%20Web!5e0!3m2!1sen!2sro!4v1568880378406!5m2!1sen!2sro"
-                width="100%"
-                height="480"
-                frameborder="0"
-                style="border:0;"
-                allowfullscreen=""
-              ></iframe>
+            <b-col class="edw_map" data-aos="fade-up" lg="6">
+              <a
+                href="https://www.google.com/maps/place/Eau+de+Web/@44.4660201,26.0824334,15z/data=!4m2!3m1!1s0x0:0xfc73471ea7cf8a74?sa=X&ved=2ahUKEwjDkpnnxv_kAhXRl4sKHf3CAjMQ_BIwE3oECA8QCA"
+                ><img src="@/assets/img/edw_map.png" alt=""
+              /></a>
             </b-col>
           </b-row>
           <!-- Contact and Map Row End -->
